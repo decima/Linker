28ee9d5775dbe3f0ef7dc79005a39f43edf18e45
decima <h.larget@gmail.com>
2023-02-18 10:12:06 +0100

    adding logs

--- src/Services/LinkManager.php
+++ src/Services/LinkManager.php
@@ -4,13 +4,15 @@ namespace App\Services;
 
 use DOMDocument;
 use DOMXPath;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Lock\LockFactory;
 
 class LinkManager
 {
 
-    public function __construct(private readonly LockFactory $lockFactory, private readonly string $baseDir = "")
+    public function __construct(private readonly LockFactory $lockFactory, private readonly LoggerInterface $logger, private readonly string $baseDir = "")
     {
         @mkdir($this->baseDir, permissions: 0777, recursive: true);
     }
@@ -18,49 +20,69 @@ class LinkManager
     public function listResources()
     {
         $finder = new Finder();
+        $this->logger->info("listing all categories");
         return $finder->files()->in($this->baseDir);
     }
 
     public function store($resourceName, $url)
     {
+
         $lock = $this->lockFactory->createLock('file-' . $resourceName);
+        $this->logger->info("locking category $resourceName", ["category" => $resourceName]);
 
         $lock->acquire(true);
         $resource = $this->getResource($resourceName);
 
         $content = [];
+
         try {
             $content = $this->getDetails($url);
         } catch (\Throwable $throwable) {
+            $this->logger->error("unable to get url content", ["category" => $resourceName, "url" => $url]);
+            $this->logger->debug($throwable->getMessage());
         }
+
         $content['url'] = $url;
         $content['storedAt'] = new \DateTime();
         unset($resource[sha1($url)]);
         $resource[sha1($url)] = $content;
         $this->updateResource($resourceName, $resource);
         $lock->release();
+        $this->logger->info("unlocking category $resourceName", ["category" => $resourceName]);
+
     }
 
     public function retrieve($resourceName)
     {
         $lock = $this->lockFactory->createLock('file-' . $resourceName);
+        $this->logger->info("locking category $resourceName", ["category" => $resourceName]);
         $lock->acquire(true);
         $content = $this->getResource($resourceName);
+
         $lock->release();
+        $this->logger->info("unlocking category $resourceName", ["category" => $resourceName]);
         return $content;
     }
 
     private function getResource($resourceName)
     {
         $content = @file_get_contents($this->baseDir . "/" . $resourceName);
+        $this->logger->debug("reading file", ["category" => $resourceName]);
+
         if ($content === null) {
             return [];
         }
-        return json_decode($content, true) ?? [];
+        $finalContent = json_decode($content, true) ?? [];
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $this->logger->warning(json_last_error_msg(), ["category" => $resourceName]);
+        }
+
+        return $finalContent;
     }
 
     private function updateResource($resourceName, $content)
     {
+        $this->logger->info("saving update into $resourceName", ["category" => $resourceName]);
         return file_put_contents($this->baseDir . "/" . $resourceName, json_encode($content));
     }
 
@@ -77,6 +99,10 @@ class LinkManager
         @$doc->loadHTML($html);
         $nodes = $doc->getElementsByTagName('title');
         $title = $nodes?->item(0)?->nodeValue;
+        if ($title === null) {
+            $this->logger->notice("no title found for $url", ["url" => $url]);
+            $title = $url;
+        }
         $xpath = new DOMXPath($doc);
         $query = '//*/meta[starts-with(@property, \'og:\')]';
         $metas = $xpath->query($query);
@@ -96,21 +122,29 @@ class LinkManager
         }
         if (isset($settings['keywords'])) {
             $settings['keywords'] = explode(',', $settings['keywords']);
+        } else {
+            $this->logger->info("no keywords find on page", ["url" => $url]);
         }
         return $settings;
     }
 
     public function delete($resourceName, $fileId)
     {
+        $this->logger->info("locking category $resourceName", ["category" => $resourceName]);
         $lock = $this->lockFactory->createLock('file-' . $resourceName);
         $lock->acquire(true);
         $content = $this->getResource($resourceName);
         unset($content[$fileId]);
+        $this->logger->info("deleted url $fileId in $resourceName", ["category" => $resourceName]);
 
         $this->updateResource($resourceName, $content);
         $lock->release();
+        $this->logger->info("unlocking category $resourceName", ["category" => $resourceName]);
+
         if (count($content) === 0) {
             $this->deleteFile($resourceName);
+            $this->logger->notice("no more content in $resourceName, deleting it", ["category" => $resourceName]);
+
         }
     }
 
